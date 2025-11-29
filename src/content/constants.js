6ab000edb435233ec38826f03a86f748927eec85
moalimirinfinity <moalimirinfinity@gmail.com>
2025-11-29 13:19:49 +0330

    feat: add content constants

---
+++ src/content/constants.js
@@ -0,0 +1,16 @@
+export const MESSAGE_SELECTOR = [
+  '[data-testid="conversation-turn"]',
+  '[data-testid^="conversation-turn-"]',
+  'article[role="presentation"]',
+  '[data-testid="chat-message"]',
+  'div[data-message-author-role]',
+  'li[data-message-author-role]'
+].join(', ');
+
+export const PERSIAN_CHAR_REGEX = /[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDFF\uFE70-\uFEFF]/;
+
+export const LANGUAGE_HINT_DEFAULT = 'english';
+export const LANGUAGE_HINT_MESSAGE_TYPE = 'GPT_ENHANCER_DETECT_LANGUAGE';
+export const LANGUAGE_DETECTION_MAX_MESSAGES = 6;
+export const LANGUAGE_DETECTION_MAX_CHARS = 800;
+export const LANGUAGE_DETECTION_CACHE_INTERVAL = 2000;
